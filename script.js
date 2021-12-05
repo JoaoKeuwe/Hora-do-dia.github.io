697ebd89ff9e50fc373a9673017aec88f96136c2
João Keuwe <91097315+JoaoKeuwe@users.noreply.github.com>
2021-12-04 21:24:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function carregar() {
   let img = window.document.getElementById("imagem");
   let data = new Date();   
   let hora = data.getHours(); 
- msg.innerHTML = `Agora são ${hora} horas.`;
+  msg.innerHTML = `Agora são ${hora} horas.`;
 
   if (hora >= 0 && hora < 12) {
     img.src = "fotomanha.png";
